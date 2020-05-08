2234cdfb0b122102306cd9b5c9327d8eaf845522
nuatmochoi <tjddn8770@naver.com>
2020-05-08 18:09:23 +0900

    Add: update popup

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,16 +1,24 @@
-module.exports = { 
-    base : '/algopress/',
-    assetsPublicPath: '/',
-    title : 'Algorithm Note',
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-        ],
-        sidebar: [
-            ['/', 'Overview'],
-            '/docs/dfs/dfs',
-            '/docs/dp/dp'
-        ]
-    },
-    plugins: ['@vuepress/back-to-top']
-}
+module.exports = {
+	base: '/algopress/',
+	assetsPublicPath: '/',
+	title: 'Algorithm Note',
+	serviceWorker: true,
+	themeConfig: {
+		nav: [{ text: 'Home', link: '/' }],
+		sidebar: [['/', 'Overview'], '/docs/dfs/dfs', '/docs/dp/dp'],
+	},
+	head: [
+		['link', { rel: 'icon', href: '/images/icon-144x144.png' }],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+	],
+	plugins: [
+		['@vuepress/back-to-top'],
+		[
+			'@vuepress/pwa',
+			{
+				serviceWorker: true,
+				updatePopup: true,
+			},
+		],
+	],
+};
